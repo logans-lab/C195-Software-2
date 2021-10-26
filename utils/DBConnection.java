26afec0c035d3b38d2212610f3ed7df2dd904ff1
Logan <72273201+logans-lab@users.noreply.github.com>
2021-10-26 16:45:05 -0500

    Update DBConnection.java

--- utils/DBConnection.java
+++ utils/DBConnection.java
@@ -14,15 +14,15 @@ public class DBConnection {
     private static final String protocol = "jdbc:";
     private static final String vendorName = "mysql:";
     private static final String ipAddress = "//sql5.freesqldatabase.com:3306/";
-    private static final String dbName = "sql5440570";
+    private static final String dbName = "sql5447022";
     private static final String autoReconnect = "?autoReconnect=true";
     private static final String jdbcURL = protocol + vendorName + ipAddress + dbName + autoReconnect;
 
     private static final String mySQLJDBCDriver = "com.mysql.cj.jdbc.Driver";
     private static Connection connection = null;
 
-    private static final String username = "sql5440570";
-    private static final String password = "WIite7JKH5";
+    private static final String username = "sql5447022";
+    private static final String password = "uqtZxtsepl";
 
     /**
      * Start connection. Connects application to related database.
